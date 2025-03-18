2fda9db5731c3577124c01b24d112cc2399a5886
Juan Pedro Lozano <jpyreaction@gmail.com>
2025-03-18 19:29:39 +0100

    Updates

---
+++ precache-manifest.2a7d5350c856da8b04df57581e08f778.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827793f4475373b8447fb88171c30f77",
+    "url": "/guitarlucia/index.html"
+  },
+  {
+    "revision": "a2cceb5f76326ae8d629",
+    "url": "/guitarlucia/static/css/main.307059bf.chunk.css"
+  },
+  {
+    "revision": "b7758ad7dbc7e926338c",
+    "url": "/guitarlucia/static/js/2.1dca0142.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/guitarlucia/static/js/2.1dca0142.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3751c406ee838da5a7d",
+    "url": "/guitarlucia/static/js/3.0bc56c22.chunk.js"
+  },
+  {
+    "revision": "a2cceb5f76326ae8d629",
+    "url": "/guitarlucia/static/js/main.44dabd6f.chunk.js"
+  },
+  {
+    "revision": "ba06ab93ef7d05a4004c",
+    "url": "/guitarlucia/static/js/runtime-main.58457d8e.js"
+  }
+]);
